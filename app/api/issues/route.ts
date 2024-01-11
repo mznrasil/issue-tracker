3595126f9635c2937ed2a49d0a1836ee772c1a99
mznrasil <mzn.rasil@gmail.com>
2024-01-11 20:54:31 +0545

    Build an API for creating issues

---
+++ app/api/issues/route.ts
@@ -0,0 +1,26 @@
+import { NextRequest } from "next/server";
+import { z } from "zod";
+import prisma from "@/prisma/client";
+
+const createIssueSchema = z.object({
+  title: z.string().min(1).max(255),
+  description: z.string().min(1),
+});
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const validation = createIssueSchema.safeParse(body);
+  if (!validation.success) {
+    return new Response(JSON.stringify({ errors: validation.error.errors }), {
+      status: 400,
+    });
+  }
+
+  const newIssue = await prisma.issue.create({
+    data: {
+      title: body.title,
+      description: body.description,
+    },
+  });
+  return new Response(JSON.stringify({ data: newIssue }), { status: 201 });
+}
